d52c5a00c3147d7ccf9d7563aed83ba795e80e70
waleedsadiq <walyed909@gmail.com>
2025-09-16 15:52:36 +0300

    save update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,8 +22,8 @@
     <link rel="stylesheet" href="{{ asset('frontand/assets/css/magnific-popup.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontand/assets/css/swiper-bundle.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontand/assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontand/assets/css/custom.css') }}">
     <link rel="stylesheet" href="{{ asset('frontand/assets/fonts/feather-font/css/iconfont.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontand/assets/css/custom.css') }}">
 
     @yield('style')
 
