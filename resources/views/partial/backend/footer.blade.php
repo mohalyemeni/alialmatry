2dc40547a1fe828791ce98ad41331e9c6e1ab35d
waleedsadiq <walyed909@gmail.com>
2025-09-22 16:29:26 +0300

    save update

--- resources/views/partial/backend/footer.blade.php
+++ resources/views/partial/backend/footer.blade.php
@@ -4,7 +4,6 @@
             <p class="text-muted mb-1 mb-md-0">Copyright © 2025 <a href="https://www.nobleui.com" target="_blank">Era
                     Tecnology</a>.
             </p>
-            <p class="text-muted">Handcrafted With <i class="mb-1 text-primary ms-1 icon-sm" data-feather="heart"></i>
-            </p>
+
         </footer>
         <!-- partial -->
