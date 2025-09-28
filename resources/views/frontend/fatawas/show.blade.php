1e1a6d671ff142f9da49d50f36f77ac830cd63a6
Walyed Sadiq <walyed909@gmail.com>
2025-09-28 18:50:07 +0300

    save all Updates

--- resources/views/frontend/fatawas/show.blade.php
+++ resources/views/frontend/fatawas/show.blade.php
@@ -190,7 +190,7 @@
                 </div>
             </div>
 
-            <aside class="col-xxl-4 col-lg-4  pb-5">
+            <aside class=" col-lg-4  pb-5">
                 <div class="card sticky-top" style="top:100px;">
                     <div class="card-body">
                         <h5 class="card-title mb-3">{{ __('panel.recent_fatawas') ?? 'أحدث الفتاوى' }}</h5>
@@ -228,7 +228,7 @@
                                                 $rd_img = asset('storage/' . ltrim($rd->img, '/'));
                                             }
                                         }
-                                        $rd_img = $rd_img ?: asset('frontand/assets/img/normal/counter-image.jpg');
+                                        $rd_img = $rd_img ?: asset('frontand/assets/img/No-Image.png');
 
                                         $rd_date = $rd->published_on
                                             ? \Carbon\Carbon::parse($rd->published_on)->format('d M, Y')
