705080211d4aa2aab48eecccb0b7ebc0cb5f56aa
Walyed Sadiq <walyed909@gmail.com>
2025-09-28 16:06:32 +0300

    save all Updates

--- resources/views/vendor/pagination/simple-tailwind.blade.php
+++ resources/views/vendor/pagination/simple-tailwind.blade.php
@@ -3,25 +3,36 @@
          aria-label="Pagination Navigation"
          class="pagination-nav w-full flex items-center justify-between px-4 flex-row-reverse"
          dir="rtl">
+
         {{-- Previous --}}
         @if ($paginator->onFirstPage())
-            <span class="prev-btn relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md dark:text-gray-600 dark:bg-gray-800 dark:border-gray-600">
-                {!! __('pagination.previous') !!}
+            <span class="th-btn style1 th-btn1 prev-btn disabled relative inline-flex items-center px-4 py-2 text-sm font-medium cursor-default leading-5 rounded-md"
+                  aria-disabled="true" style="pointer-events:none; opacity:0.6;">
+                <span class="btn-text" data-back=" السابق" data-front=" السابق">{!! __('pagination.previous') !!}</span>
+                <i class="fa-solid fa-arrow-right ms-1" aria-hidden="true"></i>
             </span>
         @else
-            <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="prev-btn relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none">
-                {!! __('pagination.previous') !!}
+            <a href="{{ $paginator->previousPageUrl() }}" rel="prev"
+               class="th-btn style1 th-btn1 prev-btn relative inline-flex items-center px-4 py-2 text-sm font-medium rounded-md focus:outline-none"
+               aria-label="{{ __('pagination.previous') }}">
+                <span class="btn-text" data-back=" السابق" data-front=" السابق">{!! __('pagination.previous') !!}</span>
+                <i class="fa-solid fa-arrow-right ms-1" aria-hidden="true"></i>
             </a>
         @endif
 
         {{-- Next --}}
         @if ($paginator->hasMorePages())
-            <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="next-btn relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none">
-                {!! __('pagination.next') !!}
+            <a href="{{ $paginator->nextPageUrl() }}" rel="next"
+               class="th-btn style1 th-btn1 next-btn relative inline-flex items-center px-4 py-2 text-sm font-medium rounded-md focus:outline-none"
+               aria-label="{{ __('pagination.next') }}">
+                <i class="fa-solid fa-arrow-left me-1" aria-hidden="true"></i>
+                <span class="btn-text" data-back=" التالي" data-front=" التالي">{!! __('pagination.next') !!}</span>
             </a>
         @else
-            <span class="next-btn relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default rounded-md">
-                {!! __('pagination.next') !!}
+            <span class="th-btn style1 th-btn1 next-btn disabled relative inline-flex items-center px-4 py-2 text-sm font-medium cursor-default leading-5 rounded-md"
+                  aria-disabled="true" style="pointer-events:none; opacity:0.6;">
+                <i class="fa-solid fa-arrow-left me-1" aria-hidden="true"></i>
+                <span class="btn-text" data-back=" التالي" data-front=" التالي">{!! __('pagination.next') !!}</span>
             </span>
         @endif
     </nav>
