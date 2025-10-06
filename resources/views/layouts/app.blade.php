d6382c511461cb714ed3020481c70aaf687ebfbe
Walyed Sadiq <walyed909@gmail.com>
2025-10-06 18:10:05 +0300

    save all Updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -125,9 +125,9 @@
             }
 
             .wa-icon {
-                font-size: 18px;
-                width: 32px;
-                height: 32px;
+                font-size: 24px;
+                width: 45px;
+                height: 45px;
             }
         }
     </style>
