4fc59696bf6620ed5eec75844b6a06d0d818303f
Walyed Sadiq <walyed909@gmail.com>
2025-10-08 15:27:42 +0300

    save all Updates

--- resources/views/frontend/sections/vedios.blade.php
+++ resources/views/frontend/sections/vedios.blade.php
@@ -109,7 +109,7 @@
                             <div class="card-body st-card-body">
                                 <h5 class="card-title st-title text-end">
                                     <a href="{{ route('frontend.videos.show', $main->slug) }}"
-                                        class="text-dark ellipsis-title a_style" title="{{ e($main->title) }}">
+                                        class="text_wihte ellipsis-title a_style" title="{{ e($main->title) }}">
                                         {{ e($smartLimit($main->title, 23)) }}
                                     </a>
                                 </h5>
