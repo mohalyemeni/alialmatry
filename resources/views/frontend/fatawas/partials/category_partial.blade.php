083dd79c6bee8e22cb40375f3196b39456eee4c7
amg102 <daiaafofo659@gmail.com>
2025-10-11 17:37:55 +0300

    save all Updates

--- resources/views/frontend/fatawas/partials/category_partial.blade.php
+++ resources/views/frontend/fatawas/partials/category_partial.blade.php
@@ -1,4 +1,4 @@
-<div class="list-group-item d-flex justify-content-between align-items-start py-3">
+<div class="list-group-item d-flex justify-content-between align-items-start py-3 fatawa-item">
     <div class="me-3" style="flex:1;">
         <h5 class="mb-1">
             <i class="fa fa-gavel me-2 text-primary"></i>
@@ -12,7 +12,7 @@
         @endif
     </div>
 
-    <div class="button-wrapp d-flex align-items-center me-4">
+    <div class="button-wrapp d-flex align-items-center">
         <a href="{{ route('frontend.fatawas.show', $fatawa->slug) }}" class="th-btn style1 th-btn1">
             <span class="btn-text" data-back=" مشاهدة" data-front=" مشاهدة"></span>
             <i class="fa-solid fa-eye me-1"></i>
